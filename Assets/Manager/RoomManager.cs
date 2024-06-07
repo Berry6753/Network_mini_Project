6e7badfc6960ca17bad215201505671a7e7f2455
WSM <wsm6753@gmail.com>
2024-06-07 16:28:21 +0900

    [feat] 룸매니저 플레이어 UI 프리팹 생성 기능 추가

--- Assets/Manager/RoomManager.cs
+++ Assets/Manager/RoomManager.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 using Mirror;
 
 public class RoomManager : NetworkRoomManager
-{ 
+{
+    public override void OnRoomServerConnect(NetworkConnectionToClient conn)
+    {
+        base.OnRoomServerConnect(conn);
 
+        var spawnPoint = FindObjectOfType<LobbySpawn>().GetSpawnPoint();
+
+        var player = Instantiate(spawnPrefabs[0], spawnPoint, Quaternion.identity, GameObject.Find("GameRoom").transform);
+        NetworkServer.Spawn(player, conn);
+    }
 }
