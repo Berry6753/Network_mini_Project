e9f664d7f74572ade70378c45d9257b1d4a75b91
WSM <wsm6753@gmail.com>
2024-06-07 16:31:46 +0900

    [feat] 방 들어오기 전에 적은 닉네임 플레이어 UI에 연동

--- Assets/Play/Scripts/PlayerSettings.cs
+++ Assets/Play/Scripts/PlayerSettings.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerSettings : MonoBehaviour
 {
     public static string nickname;
+
+    private TMP_Text nicknameText;
+    public static GameObject ReadyImg;
+
+    private void Awake()
+    {
+        nicknameText = GetComponentInChildren<TMP_Text>();
+    }
+
+    private void OnEnable()
+    {
+        nicknameText.text = nickname;
+    }
 }
