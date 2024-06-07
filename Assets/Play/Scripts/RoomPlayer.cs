099f6757d61bd3bccd4fb53c92037a131b374464
WSM <wsm6753@gmail.com>
2024-06-07 16:30:59 +0900

    [feat] RoomPlayer 스크립트에 플레이어 UI의 색 지정 기능 추가

--- Assets/Play/Scripts/RoomPlayer.cs
+++ Assets/Play/Scripts/RoomPlayer.cs
@@ -2,8 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.UI;
 
 public class RoomPlayer : NetworkRoomPlayer
 {
+    private Image playerInfoImg;
 
+    [SyncVar(hook = nameof(SetPlayerColor_Hook))]
+    public EPlayerColor playerColor;
+
+    public void SetPlayerColor_Hook(EPlayerColor oldColor, EPlayerColor newColor)
+    {
+        if (playerInfoImg == null)
+        {
+            playerInfoImg = GetComponentInChildren<Image>();
+        }
+        playerInfoImg.color = PlayerColor.GetColor(newColor);
+    }
+
+    private void Start()
+    {
+        playerInfoImg = GetComponentInChildren<Image>();
+        playerInfoImg.color = PlayerColor.GetColor(playerColor);
+    }
+
+    private void DefaultPlayerColor()
+    {
+        var roomslots = (NetworkManager.singleton as RoomManager).roomSlots;
+        EPlayerColor color = EPlayerColor.White;
+        for (int i = 0; i < (int)EPlayerColor.Lime + 1; i++)
+        { 
+            bool isFindSameColor = false;
+            foreach (var roomPlayer in roomslots) 
+            { 
+                var thisRoomPlayer = roomPlayer as RoomPlayer;
+                if (thisRoomPlayer.playerColor == (EPlayerColor)i && roomPlayer.netId != netId)
+                { 
+                    isFindSameColor = true;
+                    break;
+                }
+            }
+
+            if (!isFindSameColor)
+            {
+                color = (EPlayerColor)i;
+                break;
+            }
+        }
+        playerColor = color;
+
+        //Vector3 spawnPos = FindObjectOfType(S)
+    }
 }
