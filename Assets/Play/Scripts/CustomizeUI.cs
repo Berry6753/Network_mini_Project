ed02ff04cefc9ed2ed1d325df300dccf8686e8da
WSM <wsm6753@gmail.com>
2024-06-09 22:00:11 +0900

    [Add] CustomizeUI 스크립트 추가

---
+++ Assets/Play/Scripts/CustomizeUI.cs
@@ -0,0 +1,58 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomizeUI : MonoBehaviour
+{
+    [SerializeField] private Image playerInfo;
+
+    [SerializeField] private List<ColorSelectButton> colorSelectButtons;
+
+    private void OnEnable()
+    {
+        UpdateColorButton();
+
+        var roomSlots = (NetworkManager.singleton as RoomManager).roomSlots;
+        foreach (var player in roomSlots)
+        { 
+            var aPlayer = player as FeignRoomPlayer;
+            if (aPlayer.isLocalPlayer)
+            {
+                UpdatePreviewColor(aPlayer.playerColor);
+                break;
+            }
+        }
+    }
+
+    public void UpdateColorButton()
+    {
+        var roomSlots = (NetworkManager.singleton as RoomManager).roomSlots;
+
+        for (int i = 0; i < colorSelectButtons.Count; i++)
+        {
+            colorSelectButtons[i].SetInteractable(true);
+        }
+
+        foreach(var player in roomSlots)
+        {
+            var aPlyaer = player as FeignRoomPlayer;
+            colorSelectButtons[(int)aPlyaer.playerColor].SetInteractable(false);
+        }
+    }
+
+    public void UpdatePreviewColor(EPlayerColor color)
+    { 
+        playerInfo.color = PlayerColor.GetColor(color);
+    }
+
+    public void OnClickColorButton(int index)
+    {
+        if (colorSelectButtons[index].isInteractable)
+        {
+            FeignRoomPlayer.MyRoomPlayer.CmdSetPlayerColor((EPlayerColor)index);
+            UpdatePreviewColor((EPlayerColor)index);
+        }
+    }
+}
